ea145b1b18a8802ceb0820b8297d878729409613
Tomasz Siudut <TomekSiudut@users.noreply.github.com>
2021-01-25 22:40:06 +0100

    mario jump added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,40 @@
 import "package:flutter/material.dart";
 import 'package:mario/widgets/button.dart';
+import "dart:async";
 import 'package:mario/widgets/mario.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double time = 0;
+  double height = 0;
+  static double marioX = -1;
+  static double marioY = 1;
+  double initialHeight = marioY;
+
+  void preJump() {
+    time = 0;
+    initialHeight = marioY;
+  }
+
+  void jump() {
+    preJump();
+    Timer.periodic(Duration(milliseconds: 50), (timer) {
+      time += 0.05;
+      height = -4.9 * time * time + 5 * time;
+      if (initialHeight - height > 1) {
+        marioY = 1;
+      } else {
+        setState(() {
+          marioY = initialHeight - height;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +45,7 @@ class HomePage extends StatelessWidget {
             child: Container(
                 color: Colors.blue,
                 child: AnimatedContainer(
-                    alignment: Alignment(0, 1), duration: Duration(milliseconds: 0), child: Mario()))),
+                    alignment: Alignment(marioX, marioY), duration: Duration(milliseconds: 0), child: Mario()))),
         Expanded(
             flex: 1,
             child: Container(
@@ -23,7 +55,7 @@ class HomePage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
                       Button(child: Icon(Icons.arrow_back, color: Colors.white)),
-                      Button(child: Icon(Icons.arrow_upward, color: Colors.white)),
+                      Button(child: Icon(Icons.arrow_upward, color: Colors.white), function: jump),
                       Button(child: Icon(Icons.arrow_forward, color: Colors.white))
                     ],
                   )),
--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -3,12 +3,16 @@ import "package:flutter/material.dart";
 class Button extends StatelessWidget {
   final child;
   final color;
-  Button({this.child, this.color});
+  final Function function;
+  Button({this.child, this.color, this.function});
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-        borderRadius: BorderRadius.circular(15),
-        child: Container(padding: EdgeInsets.all(10), color: Colors.brown[300], child: child));
+    return GestureDetector(
+      onTap: function,
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Container(padding: EdgeInsets.all(10), color: Colors.brown[300], child: child)),
+    );
   }
 }
